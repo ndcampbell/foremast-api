3c950090de0f8581ba7cdbd092435df26361ebb4
Doug Campbell <dcampbell@gogoair.com>
2017-10-11 15:08:01 -0500

    added version to routes

--- src/app.py
+++ src/app.py
@@ -8,29 +8,32 @@ from rq.job import Job
 from consts import redis_url, logging_level
 from worker import run_runner
 
+
+API_VERSION='v1'
+
 logging.getLogger().setLevel(logging_level)
 app = Flask(__name__)
 conn = redis.from_url(redis_url)
 q = Queue(connection=conn)
 
-@app.route('/health', methods=['GET'])
+@app.route("/"+API_VERSION+"/health", methods=['GET'])
 def health():
     """Basic healthcheck endpoint"""
     return jsonify({"up": True}), 200
 
-@app.route('/runner/<action>', methods=['POST'])
+@app.route("/"+API_VERSION+"/runner/<action>", methods=['POST'])
 def runner(action):
     """Runs Foremast Runner against posted resources"""
     job = q.enqueue_call(func=run_runner, args=[action], kwargs=request.json, timeout=600)
-    status_url = "{}runner/status/{}".format(request.url_root, job.get_id())
+    status_url = "{}{}/runner/status/{}".format(request.url_root, API_VERSION, job.get_id())
     return jsonify({"task_id": job.get_id(), "status_url": status_url})
 
-@app.route("/runner/status/<job_key>", methods=['GET'])
+@app.route("/"+API_VERSION+"/runner/status/<job_key>", methods=['GET'])
 def get_status(job_key):
     """Gets results of task with task ID"""
     job = Job.fetch(job_key, connection=conn)
 
-    logs_url = "{}runner/logs/{}".format(request.url_root, job_key)
+    logs_url = "{}{}/runner/logs/{}".format(request.url_root, API_VERSION, job_key)
     status_dict = {"status": "", "logs_url": logs_url}
     return_code = 200
     if job.is_finished:
@@ -46,8 +49,9 @@ def get_status(job_key):
 
     return jsonify(status_dict), return_code
 
-@app.route("/runner/logs/<job_key>", methods=['GET'])
+@app.route("/"+API_VERSION+"/runner/logs/<job_key>", methods=['GET'])
 def get_logs(job_key):
+    """Returns log as plain text, easiest way to read the log"""
     job = Job.fetch(job_key, connection=conn)
     if job.is_finished:
         logs = job.result
@@ -58,7 +62,7 @@ def get_logs(job_key):
     return str(logs), 200
     
 
-@app.route("/runner/jobs", methods=['GET'])
+@app.route("/"+API_VERSION+"/runner/jobs", methods=['GET'])
 def get_all_jobs():
     """Returns list of failed_jobs and list of queued_jobs"""
     fq = get_failed_queue(connection=conn)
